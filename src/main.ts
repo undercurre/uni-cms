d428b3583ed39bcdcaa7d4f9fe709fc2c8739a92
Mr.Li <63112788+undercurre@users.noreply.github.com>
2023-06-14 15:15:48 +0800

    feat: 加入原子化css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import pinia from './stores'
+import 'virtual:windi.css'
 
 export function createApp() {
   const app = createSSRApp(App)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
+import WindiCSS from 'vite-plugin-windicss';
+import MiniProgramTailwind from '@dcasia/mini-program-tailwind-webpack-plugin/rollup';
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    WindiCSS(),
+    MiniProgramTailwind(),
     AutoImport({
       imports: ['vue', 'uni-app'],
       dts: './auto-imports.d.ts', // 安装好依赖后，重新运行编译即可自动在根目录下生成此声明文件
